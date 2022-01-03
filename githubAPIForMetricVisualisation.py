790a4135686001d387553e884ba795e55ad3de8c
Ajchan Mamedov <mamedova@tcd.ie>
2022-01-03 02:06:32 +0000

    added visualisation

--- githubAPIForMetricVisualisation.py
+++ githubAPIForMetricVisualisation.py
@@ -41,3 +41,38 @@ def getAllData(user):
         repo_names.append(repo.full_name)
         commit_info.append(commits)
         forksByRepos.append(repo.forks)
+
+    doTheVisualisation(user, repo_names, commit_info, languages_used, forksByRepos)
+
+def doTheVisualisation(user, repo_names, commit_info, languages_used, forksByRepos):
+    #overall languages used pie chart for the user(number of charactes written in each language)
+    language_pie_chart = pygal.Pie(height = 1000, width = 1000, explicit_size = 1000)
+    language_pie_chart._title = f'Languages used by {user.login}'
+    languages = languages_used.keys()
+    for language in languages:
+        language_pie_chart.add(language, languages_used[language])
+    language_pie_chart.render_in_browser()
+
+    #commits bar chart for each repository
+    commits_bar_chart = pygal.Bar(height = 1000, width = 1000, explicit_size = 1000)
+    commits_bar_chart.title = f'Commits for different Repositories by {user.login}'
+    commits_bar_chart.x_labels = map(str, repo_names)
+    commits_bar_chart.add("Commits", commit_info)
+    commits_bar_chart.render_in_browser()
+
+    #followers vs following gauge chart
+    following_gauge_chart = pygal.Gauge(height = 1000, width = 1000, explicit_size = 1000)
+    following_gauge_chart.title = f'Following vs Followers for the {user.login}'
+    following_gauge_chart.add("Following", user.following)
+    following_gauge_chart.add("Followers", user.followers)
+    following_gauge_chart.render_in_browser()
+
+    #number of times each repository was forked by other users from the user we inquired about
+    forks_line_chart = pygal.Line(height = 1000, width = 1000, explicit_size = 1000)
+    forks_line_chart.title = f'Number of forks from a repositories from {user.login}'
+    forks_line_chart.x_labels = map(str, repo_names)
+    forks_line_chart.add("Forks", forksByRepos)
+    forks_line_chart.render_in_browser()
+
+if __name__ == "__main__":
+    main()
